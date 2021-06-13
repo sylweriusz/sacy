af8f1a2ea9c965dd216df8a97113e0a68c794af7
Sylweriusz <sylweriusz@users.noreply.github.com>
2021-06-13 13:37:44 +0200

    Update Sacy.php

--- src/sacy/Sacy.php
+++ src/sacy/Sacy.php
@@ -163,6 +163,9 @@ class Sacy {
                 // see last comment
                 return $content;
             }
+            else {
+                $GLOBALS['SACY_WORKED'] = true;
+            }
         }
         $m = null;
         foreach($render as $r){
